4a9f31f525537c0d35259afd8e0d4331a285a5bd
Adarsh <codewithadarsh007@gmail.com>
2026-03-31 14:09:17 +0530

    added feed to redux store

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "feed", { withCredentials: true });
+      dispatch(addFeed(res));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(()=>{
+  getFeed();
+  },[])
+
+  return <div>Feed</div>;
+};
+
+export default Feed;

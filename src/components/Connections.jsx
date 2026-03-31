42f48d711fe1f109688b9e550b1218ec16fb64af
Adarsh <codewithadarsh007@gmail.com>
2026-03-31 23:10:27 +0530

    Built: Request Page  & Accept/Reject connection feature.

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -26,17 +26,21 @@ const Connections = () => {
 
   if (!connections) return;
 
-  if (connections.length === 0) return <h1>No Connections Found</h1>;
+  if (connections.length === 0)
+    return <h1 className="flex justify-center my-10">No Connections Found</h1>;
 
   return (
-    <div className="text-center my-10">
+    <div className="text-center  my-10">
       <h1 className="text-bold text-white text-3xl"> Connections</h1>
       {connections.map((connection) => {
         const { _id, firstName, lastName, photoURL, age, gender, about } =
           connection;
 
         return (
-          <div key={_id}   className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+          <div
+            key={_id}
+            className="  flex justify-center items-center  m-4 p-4 rounded-lg bg-base-300 w-1/3 mx-auto"
+          >
             <div>
               <img
                 alt="photo"
---
+++ src/utils/requestsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const requestsSlice = createSlice({
+
+    name: "requests",
+    initialState: [],
+    reducers: {
+
+        addRequest: (state,action)=> action.payload,
+        removeRequest: (state,action)=>{
+            const newArray = state.filter((r)=> r._id !== action.payload );
+            return newArray;
+        }
+    }
+})
+
+
+export const{addRequest,removeRequest} = requestsSlice.actions;
+
+export default requestsSlice.reducer;

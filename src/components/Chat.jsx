76d0bcea9f7364725010c0567c71d6e349e0c085
Adarsh <codewithadarsh007@gmail.com>
2026-04-26 03:01:27 +0530

    completed chat functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,72 +1,72 @@
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import { createSocketConnection } from "../utils/socket";
-// import { useSelector } from "react-redux";
-// import axios from "axios";
-// import { BASE_URL } from "../utils/constants";
+import { createSocketConnection } from "../utils/socket";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const Chat = () => {
   const { targetUserId } = useParams();
   const [messages, setMessages] = useState([{text:"Hello"}]);
-//   const [newMessage, setNewMessage] = useState("");
-//   const user = useSelector((store) => store.user);
-//   const userId = user?._id;
+  const [newMessage, setNewMessage] = useState("");
+  const user = useSelector((store) => store.user);
+  const userId = user?._id;
 
-//   const fetchChatMessages = async () => {
-//     const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
-//       withCredentials: true,
-//     });
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
 
-//     console.log(chat.data.messages);
+    console.log(chat.data.messages);
 
-//     const chatMessages = chat?.data?.messages.map((msg) => {
-//       const { senderId, text } = msg;
-//       return {
-//         firstName: senderId?.firstName,
-//         lastName: senderId?.lastName,
-//         text,
-//       };
-//     });
-//     setMessages(chatMessages);
-//   };
-//   useEffect(() => {
-//     // eslint-disable-next-line react-hooks/set-state-in-effect
-//     fetchChatMessages();
-//   }, []);
+    const chatMessages = chat?.data?.messages.map((msg) => {
+      const { senderId, text } = msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text,
+      };
+    });
+    setMessages(chatMessages);
+  };
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    fetchChatMessages();
+  }, []);
 
-//   useEffect(() => {
-//     if (!userId) {
-//       return;
-//     }
-//     const socket = createSocketConnection();
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const socket = createSocketConnection();
     // As soon as the page loaded, the socket connection is made and joinChat event is emitted
-    // socket.emit("joinChat", {
-    //   firstName: user.firstName,
-    //   userId,
-    //   targetUserId,
-    // });
+    socket.emit("joinChat", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+    });
 
-    // socket.on("messageReceived", ({ firstName, lastName, text }) => {
-    //   console.log(firstName + " :  " + text);
-    //   setMessages((messages) => [...messages, { firstName, lastName, text }]);
-    // });
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
+      console.log(firstName + " :  " + text);
+      setMessages((messages) => [...messages, { firstName, lastName, text }]);
+    });
 
-//     return () => {
-//       socket.disconnect();
-//     };
-//   }, [userId, targetUserId]);
+    return () => {
+      socket.disconnect();
+    };
+  }, [userId, targetUserId]);
 
-//   const sendMessage = () => {
-//     const socket = createSocketConnection();
-//     socket.emit("sendMessage", {
-//       firstName: user.firstName,
-//       lastName: user.lastName,
-//       userId,
-//       targetUserId,
-//       text: newMessage,
-//     });
-//     setNewMessage("");
-//   };
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userId,
+      targetUserId,
+      text: newMessage,
+    });
+    setNewMessage("");
+  };
 console.log(targetUserId);
   return (
     <div className="w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
@@ -76,16 +76,16 @@ console.log(targetUserId);
           return (
             <div
               key={index}
-            //   className={
-            //     "chat " +
-            //     (user.firstName === msg.firstName ? "chat-end" : "chat-start")
-            //   }
+              className={
+                "chat " +
+                (user.firstName === msg.firstName ? "chat-end" : "chat-start")
+              }
 
-            className="chat chat-start"
+            // className="chat chat-start"
             >
               <div className="chat-header">
-                {/* {`${msg.firstName}  ${msg.lastName}`} */}
-                SP Aslam
+                {`${msg.firstName}  ${msg.lastName}`}
+                {/* SP Aslam */}
                 <time className="text-xs opacity-50"> 2 hours ago</time>
               </div>
               <div className="chat-bubble">You were the chosen one</div>
@@ -96,12 +96,12 @@ console.log(targetUserId);
       </div>
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
         <input
-        //   value={newMessage}
-        //   onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
           className="flex-1 border border-gray-500 text-white rounded p-2"
         ></input>
         <button 
-        // onClick={sendMessage} 
+        onClick={sendMessage} 
         className="btn btn-secondary">
           Send
         </button>
